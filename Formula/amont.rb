f1c75e1431f728f5d5adbbe2d33939a8ad849394
Frederic R. <zougi.fr@gmail.com>
2026-08-04 10:01:27 +0200

    chore: amont 1.3.0

--- Formula/amont.rb
+++ Formula/amont.rb
@@ -7,26 +7,26 @@
 class Amont < Formula
   desc "Git hooks that catch the bad commit before it exists — en amont"
   homepage "https://github.com/fredericrous/amont"
-  version "1.2.1"
+  version "1.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/fredericrous/amont/releases/download/v1.2.1/amont-1.2.1-aarch64-apple-darwin.tar.gz"
-      sha256 "8eead079fbecc3ac4bc889ff697c715cbc6dc14cadb0864d0e82c6a632b02d89"
+      url "https://github.com/fredericrous/amont/releases/download/v1.3.0/amont-1.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "b2923b920c11a40a759de142276e2fa94247524efd566992de350e4c6d601e62"
     else
-      url "https://github.com/fredericrous/amont/releases/download/v1.2.1/amont-1.2.1-x86_64-apple-darwin.tar.gz"
-      sha256 "4efd55fd1337c118dd225f0ce3df12baa07f2855cdfa05bcba62626049a593ab"
+      url "https://github.com/fredericrous/amont/releases/download/v1.3.0/amont-1.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "e1b7dfc5112bc767ba5cb4d84c0b0ad053372ad84f012922f62bfca501269fe7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/fredericrous/amont/releases/download/v1.2.1/amont-1.2.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "b6ca6223439113f4d2df8777aa5df5bedd3b9088854b724c1398071576ec18e7"
+      url "https://github.com/fredericrous/amont/releases/download/v1.3.0/amont-1.3.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "c25e0f96fd64cdcd4197e21a12ce2e93eca8a99f5ac2f36589265a7aa9eb78cb"
     else
-      url "https://github.com/fredericrous/amont/releases/download/v1.2.1/amont-1.2.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "7811e09071eb1d58b96a9ba0b7d1d04d8334d9c542fcd87fe66b0292bb13b547"
+      url "https://github.com/fredericrous/amont/releases/download/v1.3.0/amont-1.3.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "0ba24b24773985997830fe700d055e0447c86b817d6df894b11f56dc524772be"
     end
   end
 
